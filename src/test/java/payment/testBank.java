9298e4ca775d8545f2b5a27cde53ef86eab16996
M23081998 <manojarumugam2308@gmail.com>
2020-02-25 20:09:00 +0530

    update

--- src/test/java/payment/testBank.java
+++ src/test/java/payment/testBank.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 import java.util.Scanner;
 
 import CancelBooking.testCancelBooking;
-import creditcard.PaymentResponse;
 import logger.Logger;
 
 public class testBank {
